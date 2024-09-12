7d37a6361e3318ffd2b65af0c664b5de5d4a76dc
Wilian Goncalves <williangoncalvespp@hotmail.com>
2024-09-11 21:14:14 -0300

    validate data

--- main.js/script.js
+++ main.js/script.js
@@ -1,1 +1,34 @@
-const pegarItens = document.getElementsByTagName("a");
+const contactData = [];
+
+function validarCampos() {
+  //pega os inputs do form//
+  const getName = document.querySelector("#name").value;
+  const getAddress = document.querySelector("#address").value;
+  const getTelphone = document.querySelector("#telphone").value;
+  const getEmail = document.querySelector("#email").value;
+  const getDate = document.querySelector("#date").value;
+
+  // pegar os check box e radio//
+
+  const id = contactData.length + 1;
+
+  personData = {
+    id: id,
+    name: getName,
+    address: getAddress,
+    telphone: getTelphone,
+    email: getEmail,
+    birthdat: getDate,
+  };
+
+  contactData.push(personData);
+}
+
+//insere os dados no array
+
+const getSendButton = document.querySelector("#send");
+
+getSendButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  validarCampos();
+});
